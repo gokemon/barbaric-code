ecd32a0a5558dfed7866dff0d72be44feba3f89e
gokemon <michael.of.gokey@gmail.com>
2017-04-15 16:09:05 -0400

    added routes, but havent updated the  root @NgModule

--- barbaric-angular-frontend/src/app/app.component.ts
+++ barbaric-angular-frontend/src/app/app.component.ts
@@ -1,10 +1,46 @@
+// Import the Component decorator
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // We'll call our root component daily-deals
+  selector: 'daily-deals',
+  template: `
+  <div class="container">
+    <nav class="navbar navbar-default">
+        <div class="navbar-header">
+          <a class="navbar-brand" routerLink="/dashboard"></a>
+        </div>
+        <!-- On the left side of our navbar we'll display the two links for public and private deals -->
+        <ul class="nav navbar-nav">
+          <li>
+            <a routerLink="/deals" routerLinkActive="active">Deals</a>
+          </li>
+          <li>
+            <a routerLink="/special" routerLinkActive="active">Private Deals</a>
+          </li>
+        </ul>
+        <!-- On the right side of our navbar we'll display the login and logout actions depending on user state -->
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a>Log In</a>
+          </li>
+          <li>
+            <a>Log Out</a>
+          </li>
+        </ul>
+    </nav>
+    <div class="col-sm-12">
+      <!-- The router-outlet directive will display the component based on the route we are on, more on this soon -->
+      <router-outlet></router-outlet>
+    </div>
+  </div>
+  `,
+  // We'll add an inline style to properly display the navbar
+  styles : ['.navbar-right { margin-right: 0px !important}']
 })
 export class AppComponent {
-  title = 'My Barbaric Angular App!';
+
+  title = 'Daily Deals';
+
+  constructor() {}
 }
--- barbaric-angular-frontend/src/app/deal.ts
+++ barbaric-angular-frontend/src/app/deal.ts
@@ -1,2 +1,9 @@
 /* deal.ts holds our deal class which will 
 let Angular 2 know the structure of a deal. */
+export class Deal {
+  id: number;
+  name: string;
+  description: string;
+  originalPrice: number;
+  salePrice: number;
+}
